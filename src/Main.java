ea3a32bd5f72443a288eb74a322a8f98093ba487
Nacho Diez <diezgarcianacho@gmail.com>
2024-09-30 00:56:17 +0200

    ¿Ser par o no ser?

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Dime un numero!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int inumero = sc.nextInt();
+
+        int i;
+        System.out.println(inumero % 2 == 0 ? "Es par":"Es inpar");
+    }
+}
+
